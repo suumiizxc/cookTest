c2f8615fbb50625b6e289761791edc410dc74e9d
Suumii <ch.sumyadorj@gmail.com>
2021-01-04 14:13:01 +0800

    git initial

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry : "./src/js/index.js",
+    output : {
+        filename : "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    devServer: {
+        contentBase:"./dist",
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({  // Also generate a test.html
+          filename: 'index.html',
+          template: 'src/index.html'
+        })
+      ]
+}
