e4982b29a82cbbc22ab5f9b5372407964ba61cda
Suumii <ch.sumyadorj@gmail.com>
2021-01-04 22:24:28 +0800

    created search service

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,36 @@ require("@babel/polyfill");
 
 import Search from './model/Search';
 
-let search = new Search('pasta');
+/*
+    Webapp query, үр дүн
+        - Хайлтын query, үр дүн
+        - Тухайн үзүүлж байгаа жор
+        - Лайкалсан жорууд
+        - Захиалж байгаа жорууд
+*/
 
-search.doSearch().then((result)=> console.log(result));
+const state = {};
+
+const controlSearch = async() => {
+    // 1. Web-ээс хайлтын түлхүүр үгийг гаргаж авна
+    const query = 'pizza';
+
+    if(query){
+        // 2. Шинээр хайлтын обьектийг гаргаж авна
+        state.search = new Search(query);
+
+        // 3. Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ
+        // 4. Хайлтыг гүйцэтгэнэ
+        await state.search.doSearch();
+        // 5. Хайлтын үр дүнг дэлгэцэн үзүүлнэ
+        
+        console.log(state.search.recipes);
+    }
+}
+
+
+document.querySelector(".search").addEventListener("submit", (e)=>{
+    e.preventDefault();
+    controlSearch();
+})
+
